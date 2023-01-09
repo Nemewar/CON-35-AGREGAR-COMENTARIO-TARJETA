fd2ee3d7d8726fcc6eca7e48a6c5dde3e64a032a
Frank Pizarro <92424277+Nemewar@users.noreply.github.com>
2023-01-08 22:28:23 -0500

    update stack of modals

--- src/app/components/lista-usuariosc/lista-usuariosc.component.ts
+++ src/app/components/lista-usuariosc/lista-usuariosc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, IonRouterOutlet } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-usuariosc',
@@ -7,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaUsuarioscComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController,
+    private routerOutlet:IonRouterOutlet) { }
 
   ngOnInit() {}
+
+  // async mostrarModal() {
+  //   const modal = await this.modalCtrl.create({
+  //     component: ListaUsuariosPage,
+  //     swipeToClose: true,
+  //     presentingElement: this.routerOutlet.nativeEl
+  //   });
+  //   return await modal.present();
+  // }
+
+  async ocultarModal(){
+    this.modalCtrl.dismiss();
+  }
 
 }
